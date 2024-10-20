6bd1ba2aa7fbd093c8caaa2fc1e57b9ab9d70be0
lashameskhidze <lashameskhidze0@gmail.com>
2024-10-20 06:56:12 -0700

    js N1

---
+++ script.js
@@ -0,0 +1,22 @@
+// 
+const firstname = "ლაშა"; 
+console.log(firstname);   
+
+const surname = "მესხიძე"; 
+console.log(surname); 
+
+let age = 25;               
+console.log(age);
+
+const email = "lashameskhidze@gmail.com";
+console.log(email); 
+
+const phone = "555223366";     
+console.log("%c" + phone, "color:red")
+// console.log(typeof phone, "color:red")
+
+const adult = true;          
+console.log("%c" + adult, "color: yellow")
+
+
+
